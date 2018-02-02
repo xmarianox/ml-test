8d7a25fad9cd2c8d6439ca72c2a56672f7804e2a
Mariano <mariano@contenidos-digitales.com>
2018-02-02 12:22:02 -0300

    Initial commit & setup ENV

---
+++ pages/_document.js
@@ -0,0 +1,66 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+    static getInitialProps({ renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props =>
+            sheet.collectStyles(<App {...props} />),
+        );
+        const styleTags = sheet.getStyleElement();
+        return { ...page, styleTags };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>
+                    <meta charSet="utf-8" />
+                    {/* <link rel="dns-prefetch" href={app.url} />
+                    <link rel="preconnect" href={app.url} />
+
+                    <title>{app.title}</title> */}
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+
+                    {this.props.styleTags}
+
+                    {/* <link rel="manifest" href="" />
+
+                    <meta name="msapplication-TileColor" content="#161616" />
+                    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                    
+                    <meta name="format-detection" content="telephone=no" />
+                    <meta httpEquiv="x-rim-auto-match" content="none" />
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    <meta name="theme-color" content="#050509" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+                    <meta name="apple-mobile-web-app-title" content={app.title} />
+                    <meta name="msapplication-TileImage" content={app.title} />
+                    <meta name="description" content={app.description} />
+                    <meta name="keywords" content="" />
+                    <meta name="author" content="marianomolina.xyz" />
+
+                    <meta property="fb:app_id" content="" />
+                    <meta property="og:title" content={app.title} />
+                    <meta property="og:site_name" content={app.title} />
+                    <meta property="og:url" content={app.url} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:description" content={app.description} />
+                    <meta property="og:locale " content="es_LA" />
+                    <meta property="og:image" content="" />
+
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:site" content={app.url} />
+                    <meta name="twitter:creator" content={app.url} />
+                    <meta name="msapplication-config" content="/static/images/icons/browserconfig.xml" />
+                    <meta name="theme-color" content="#050509" /> */}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,1 @@
+export default () => <h1>Meli text</h1>
